ac365c5e39b391da5160321f042df5b683ebe06d
Muhammad Farooq <farooqayub646@gmail.com>
2024-11-19 16:13:50 +0500

    updated code

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,7 @@
 import "@/styles/globals.css";
+import '@fontsource-variable/libre-franklin';
+import '@fontsource/poppins';
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
